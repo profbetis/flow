7947009cbab346adb72269ccb7521efb505b3380
Kevin Weber <mrbetis@gmail.com>
2016-02-23 21:27:30 -0500

    Install and enable Thunk middleware

--- src/create-store.js
+++ src/create-store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk';
 import * as reducers from './reducers'
 
 export default function(data) {
     var finalReducer = combineReducers( reducers )
-    //var finalCreateStore = applyMiddleware( )(createStore)
-    var store = createStore( finalReducer )
+    var finalCreateStore = applyMiddleware( thunk )( createStore )
+    var store = createStore( finalCreateStore )
 
     return store
 }
