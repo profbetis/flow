0902b16e242247f30c5ac6b8232b3f88a05d7407
Kevin Weber <mrbetis@gmail.com>
2016-03-01 14:51:54 -0500

    Continue new task functionality

--- src/actions/main.js
+++ src/actions/main.js
@@ -8,3 +8,13 @@ export const toggleTodo = (listId, taskId) => {
         taskId: taskId
     }
 }
+
+export const newTodo = (title, /*dueDate, dueTime,*/ listId) => {
+    return{
+        type: 'NEW_TODO',
+        title: title,
+        dueDate: "Sometime",
+        dueTime: "1200",
+        listId: listId
+    }
+}
